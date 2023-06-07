9b6f637e22d2e0f426a4bc88cf89ff52d1a6f72b
Fabio Diogo <fabindiogo@gmail.com>
2023-06-07 09:33:41 -0300

    banco conectado com URI

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,14 +1,14 @@
-import connect from 'next-connect';
-import Joi from 'joi';
+import createHandler from '../../lib/middlewares/nextConnect'
+
 import validate from '../../lib/middlewares/validation'
 import { signupUser } from "../../modules/user/user.service";
 import { signupSchema } from "../../modules/user/user.schema"
 
+const signup = createHandler()
 
-  const signup = connect()
-    .post(validate({body: signupSchema}),(req, res) => {
-      signupUser(req.body)
-      res.status(200).json({teste: "okokokok"})
-  })
+signup.post(validate({body: signupSchema}),(req, res) => {
+    signupUser(req.body)
+    res.status(200).json({teste: "okokokok"})
+})
 
 export default signup;
