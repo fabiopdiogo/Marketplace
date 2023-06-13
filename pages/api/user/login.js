fe0189b5a92ad839b1d9a433398e741fe83b37d9
Fabio Diogo <fabindiogo@gmail.com>
2023-06-13 09:37:59 -0300

    criado token de login e cadastro

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,20 +1,29 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import createHandler from '../../../lib/middlewares/nextConnect'
 import validate from '../../../lib/middlewares/validate'
 import { login } from '../../../modules/user/user.service'
+
 import { loginSchema } from '../../../modules/user/user.schema'
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const handler = createHandler()
 
 handler.post(validate({ body : loginSchema }), async (req, res) => {
   try{
     const user = await login(req.body)
-    res.send(user)
+    req.session.user = {
+      id: user._id,
+      email: user.email
+    }
+    await req.session.save()
+    res.send({ ok: true })
+
   } catch(err) {  
     console.error(err)
     throw err
   }
 })
 
-export default handler
+export default withIronSessionApiRoute(handler, ironConfig)
--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,19 +1,26 @@
 import createHandler from '../../../lib/middlewares/nextConnect'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import validate from '../../../lib/middlewares/validate'
 import { signupUser } from "../../../modules/user/user.service";
 import { signupSchema } from "../../../modules/user/user.schema"
+
+import { ironConfig } from '../../../lib/middlewares/ironSession'
 
 const signup = createHandler()
 
 signup.post(validate({body: signupSchema}), async (req, res) => {
     try{
         const user = await signupUser(req.body)
-        res.status(201).json(user)
+        req.session.user = {
+            id: user._id,
+            email: user.email,
+        }
+        res.status(201).json({ok: true})
     }catch(err){
         console.error(err)
         throw err
     }
 })
 
-export default signup;
+export default withIronSessionApiRoute(signup, ironConfig);
